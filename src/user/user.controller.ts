3cb911af08c04051329b80e8823c74874e1650c1
SamuelAlvianM <oensam07@gmail.com>
2024-07-29 16:19:05 +0700

    update table relation 1

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards, Get, Post, Body, HttpCode, HttpStatus, Logger } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserDto } from './dto/user.dto';
+import { UpdateDto } from './dto/user.dto';
+import { User } from '@prisma/client';
+import { AuthGuard } from '@nestjs/passport';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private prisma: PrismaService) {}
+
+
+
+}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { UserService } from './user.service';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [UserController],
+  providers: [UserService]
 })
 export class UserModule {}
