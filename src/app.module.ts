4f40b557503a0bf552d283dc3d089ab6d9fed349
SamuelAlvianM <oensam07@gmail.com>
2024-07-29 15:33:36 +0700

    debugging1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModuleTsModule } from './auth/auth.module';
+import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserService } from './user/user.service';
 import { UserModule } from './user/user.module';
+import { PrismaService } from './prisma/prisma.service';
+import { ConfigModule } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [AuthModuleTsModule, PrismaModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal:true,
+    }),
+    AuthModule, 
+    PrismaModule, 
+    UserModule,
+    PassportModule.register({ defaultStrategy: 'jwt'}),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET_KEY,
+      signOptions: { expiresIn: '60m'},
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService, UserService],
+  providers: [AppService, UserService, PrismaService],
 })
 export class AppModule {}
