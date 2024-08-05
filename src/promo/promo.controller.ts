bd00b0819f4e892259809c4c3c81fa4db925384a
Sharon Silalahi <aljeazsharon@gmail.com>
2024-08-05 20:06:55 +0700

    fix. Promo for CRUD

--- src/promo/promo.controller.ts
+++ src/promo/promo.controller.ts
@@ -1,10 +1,10 @@
 /* eslint-disable prettier/prettier */
 // src/controller/promo.controller.ts
 
-import { Controller, Get, Post, Body, Param, Patch, Delete, HttpStatus, HttpCode, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Patch, Delete, HttpStatus, HttpCode, UseGuards, ParseIntPipe, NotFoundException } from '@nestjs/common';
 import { PromoService } from './promo.service';
 import { CreatePromoDto, UpdatePromoDto } from './dto/promo.dto';
-import { Role } from '@prisma/client';
+import { Promo, Role } from '@prisma/client';
 import { RolesGuard } from 'src/utils/guard/roles.guard';
 import { Roles } from 'src/utils/decorator/roles.decorator';
 import { ApiResponse, ApiBadRequestResponse, ApiUnauthorizedResponse, ApiBearerAuth } from '@nestjs/swagger';
@@ -19,8 +19,8 @@ export class PromoController {
   @Post()
   @Roles(Role.SUPER_ADMIN, Role.OWNER)
   @HttpCode(HttpStatus.CREATED)
-  @ApiResponse( {status: 201, description: 'Successfully created'})
-  @ApiBadRequestResponse({status: 400, description: 'Invalid data'})
+  @ApiResponse( {status: 201, description: 'Promo Data Created Successfully!'})
+  @ApiBadRequestResponse({status: 400, description: 'Invalid Data'})
   @ApiUnauthorizedResponse({ status: 401, description: 'Unauthorized' })
   @ApiBearerAuth()
     async createPromo(@Body() createPromoDto: CreatePromoDto) {
@@ -30,23 +30,60 @@ export class PromoController {
         }
       }
 
+  // @Get()
+  // findAll() {
+  //   return this.promoService.getAllPromos();
+  // }
+
+  @Roles(Role.SUPER_ADMIN, Role.OWNER, Role.STAFF)
+  @HttpCode(HttpStatus.OK)
+  @ApiResponse({status: 200, description: 'Fetch Data Promo Success'})
+  @ApiBadRequestResponse({status: 400, description: 'Invalid Data'})
+  @ApiUnauthorizedResponse({ status: 401, description: 'Unauthorized' })
+  @ApiBearerAuth()
   @Get()
-  findAll() {
+  async getAllPromos(): Promise<Promo[]>{
     return this.promoService.getAllPromos();
   }
 
+  @Roles(Role.SUPER_ADMIN, Role.OWNER, Role.STAFF)
+  @HttpCode(HttpStatus.OK)
+  @ApiResponse({status: 200, description: 'Fetch Data Promo Success'})
+  @ApiBadRequestResponse({status: 400, description: 'Invalid Data'})
+  @ApiUnauthorizedResponse({ status: 401, description: 'Unauthorized' })
+  @ApiBearerAuth()
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.promoService.getPromoById(+id);
+  async getPromoById(@Param('id', ParseIntPipe) id: number): Promise<Promo> {
+    return this.promoService.getPromoById(id);
   }
 
+  @Roles(Role.SUPER_ADMIN, Role.OWNER)
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiResponse({ status: 201, description: 'Data Promo Successfully Deleted!' })
+  @ApiBadRequestResponse({status: 404, description: 'Not Found'})
+  @ApiUnauthorizedResponse({ status: 401, description: 'Unauthorized' })
+  @ApiBearerAuth()
   @Patch(':id')
   update(@Param('id') id: string, @Body() updatePromoDto: UpdatePromoDto) {
     return this.promoService.updatePromo(+id, updatePromoDto);
   }
 
+  @Roles(Role.SUPER_ADMIN, Role.OWNER)
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiResponse({ status: 201, description: 'Data Promo Successfully Deleted!' })
+  @ApiBadRequestResponse({status: 404, description: 'Not Found'})
+  @ApiUnauthorizedResponse({ status: 401, description: 'Unauthorized' })
+  @ApiBearerAuth()
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.promoService.deletePromo(+id);
+  async softDeletePromo(@Param('id') id: string) {
+    const promos = await this.promoService.softDeletePromo(+id);
+    if (!promos) {
+      throw new NotFoundException("Promo Data Not Found!")
+    }
+    return {
+      statusCode: HttpStatus.NO_CONTENT,
+      message: "Delete Data Promo Success!",
+      data: promos,
+    }
   }
 }
