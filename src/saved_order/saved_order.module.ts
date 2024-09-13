64fc23439b2ee19256f086b744a87212ddbcddbd
SamuelAlvianM <oensam07@gmail.com>
2024-09-13 14:30:03 +0700

    debug imports and exports

--- src/saved_order/saved_order.module.ts
+++ src/saved_order/saved_order.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { SavedOrderService } from './saved_order.service';
 import { SavedOrderController } from './saved_order.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { TransactionModule } from 'src/transactions/transaction.module';
 
 @Module({
   controllers: [SavedOrderController],
   providers: [SavedOrderService, PrismaService],
+  exports: [SavedOrderService],
 })
 export class SavedOrderModule {}
