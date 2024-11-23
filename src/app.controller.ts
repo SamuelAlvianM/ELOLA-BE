14a30f87a071c9658d74f1386fc169fddb6de386
SamuelAlvianM <oensam07@gmail.com>
2024-11-23 19:30:28 +0700

    add customization on app.controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -9,4 +9,11 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Post()
+  sayHello(@Body() body:any): string {
+    return {...body};
+  }
+
+
 }
